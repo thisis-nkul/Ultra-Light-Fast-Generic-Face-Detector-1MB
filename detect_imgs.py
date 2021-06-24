dd700771f2c01570f708625eceabd8d146d04378
Nakul Sharma <nakul120.ns@gmail.com>
2021-06-24 20:11:32 +0530

    Update detect_imgs.py
    Updated to take `txt_file` as input which should contain a list of folders from which we want to extract the images. This is done to make it work for multiple directories at once with minimum overhead.
    
    P.S.:  The `txt_file` must contain a blank line at the end

--- detect_imgs.py
+++ detect_imgs.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--net_type', default="RFB", type=str,
                     help='The network architecture ,optional: RFB (higher precision) or slim (faster)')
-parser.add_argument('--input_size', default=640, type=int,
+parser.add_argument('--input_size', default=1280, type=int,
                     help='define network input size,default optional value 128/160/320/480/640/1280')
 parser.add_argument('--threshold', default=0.6, type=float,
                     help='score threshold')
@@ -24,6 +24,8 @@ parser.add_argument('--path', default="imgs", type=str,
                     help='imgs dir')
 parser.add_argument('--test_device', default="cuda:0", type=str,
                     help='cuda:0 or cpu')
+parser.add_argument('--txt_file', default='/content/folders.txt', type=str,
+                    help='txt file containing paths of folders')
 args = parser.parse_args()
 define_img_size(args.input_size)  # must put define_img_size() before 'import create_mb_tiny_fd, create_mb_tiny_fd_predictor'
 
@@ -52,21 +54,43 @@ net.load(model_path)
 
 if not os.path.exists(result_path):
     os.makedirs(result_path)
-listdir = os.listdir(args.path)
-sum = 0
-for file_path in listdir:
-    img_path = os.path.join(args.path, file_path)
-    orig_image = cv2.imread(img_path)
-    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
-    boxes, labels, probs = predictor.predict(image, args.candidate_size / 2, args.threshold)
-    sum += boxes.size(0)
-    for i in range(boxes.size(0)):
-        box = boxes[i, :]
-        cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
-        # label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
-        label = f"{probs[i]:.2f}"
-        # cv2.putText(orig_image, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.putText(orig_image, str(boxes.size(0)), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.imwrite(os.path.join(result_path, file_path), orig_image)
-    print(f"Found {len(probs)} faces. The output image is {result_path}")
-print(sum)
+
+paths = []
+
+with open(args.txt_file) as f:
+  print('Directories:')
+  for line in f.readlines():
+    l = line[:-1]
+    print(l)
+    paths.append(l)
+
+print('\n')
+
+for pth in paths:
+
+  listdir = os.listdir(pth)
+  sum = 0
+  for file_path in listdir:
+        img_path = os.path.join(pth, file_path)
+        orig_image = cv2.imread(img_path)
+        image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+        boxes, labels, probs = predictor.predict(image, args.candidate_size / 2, args.threshold)
+        sum += boxes.size(0)
+        for i in range(boxes.size(0)):
+            box = boxes[i, :]
+            box = [int(x) for x in box]
+            face_im = orig_image[box[1]:box[3],box[0]:box[2], :]
+            s = face_im.shape
+            if s[0]*s[1]*s[2] != 0:
+              face_im = cv2.resize(face_im, (160, 160))
+              fn, ext = file_path.split('.')[0], '.jpg'
+              cv2.imwrite(result_path + '/' + fn + '_' + str(i) + ext, face_im)
+            #cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
+            # label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
+            #label = f"{probs[i]:.2f}"
+            # cv2.putText(orig_image, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            
+        #cv2.putText(orig_image, str(boxes.size(0)), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        #cv2.imwrite(os.path.join(result_path, file_path), orig_image)
+        print(f"Found {len(probs)} faces. Currently in {pth}")
+  print(sum)
